7b493af0c190b8344f2a8bfc16d9a0f677298e4e
attila <devanddesign@protonmail.com>
2022-07-25 21:29:20 +0300

    set id of cards dynamically

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import {useState} from 'react'
 
 export default function Overview({sortMode, childToParent, cardData}) {
+    const [isActive, setActive] = useState("false");
+
+    const ToggleClass = () => {
+        setActive(!isActive);
+    };
+
     //List all cards & passed function from parent component
     const listCards = cardData.map((card) =>
-    <li key={card.id} onClick={() => childToParent(card.id)}
-    className='card'>
+    <li key={card.id} onClick={() => childToParent(card.id) }
+    className='card' id={`card-`+card.id}>
         <p className='card-text'>{card.real_name}<br/>
         {card.player_name}<br/>
         {card.asset}</p>
@@ -20,7 +26,7 @@ export default function Overview({sortMode, childToParent, cardData}) {
     //List cards in ascending order & passed function from parent component
     const listAsc = sortAscending.map((card) =>
     <li key={card.id} onClick={() => childToParent(card.id)}
-    className='card'>
+    className='card' id={`card-`+card.id}>
         <p className='card-text' id={card.id}>{card.real_name}<br/>
         {card.player_name}<br/>
         {card.asset}</p>
@@ -35,7 +41,7 @@ export default function Overview({sortMode, childToParent, cardData}) {
     //List cards in descending order & passed function from parent component
     const listDesc = sortDescending.map((card) =>
     <li key={card.id} onClick={() => childToParent(card.id)}
-    className='card'>
+    className='card' id={`card-`+card.id}>
         <p className='card-text'>{card.real_name}<br/>
         {card.player_name}<br/>
         {card.asset}</p>
